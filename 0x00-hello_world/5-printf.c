86ef0ba133df60fbef8f6bc189fc62bbd4724325
Samjnr1995 <suduu1995@gmail.com>
2022-03-12 13:22:56 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - the main function is the house of the the program, it obeys the command printf output
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art\n");
-return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
