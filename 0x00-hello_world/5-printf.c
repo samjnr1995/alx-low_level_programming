a3c12a6aceb327759a1248560be812e603974dd6
Samjnr1995 <suduu1995@gmail.com>
2022-03-12 13:05:40 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #!/bin/bash
 #include <stdio.h>
    int main (void){
-	   puts("\"prpgramming is like building a multilingual puzzle");
+	   puts("\"programming is like building a multilingual puzzle");
 	   return 0;
    }
