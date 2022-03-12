cdf84a52b3b480eb339a924c59795b70a0c42d8e
Samjnr1995 <suduu1995@gmail.com>
2022-03-12 13:15:17 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
-#!/bin/bash
 #include <stdio.h>
-   int main (void){
-	   puts("\"programming is like building a multilingual puzzle");
-	   return 0;
-   }
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art\n");
+return 0;
+}
