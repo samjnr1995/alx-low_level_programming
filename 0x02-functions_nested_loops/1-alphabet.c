8e808f0ea0ca936eff90c7b4f68c645a43953404
Samjnr1995 <suduu1995@gmail.com>
2022-03-15 16:59:54 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * main - check the code
+ * Return Always 0
+ */
+void print_alphabet(void);
+{
+char alphabet;
+alphabet= 'a';
+_putchar("Lowercase alphabets:\n");
+while(alphabet<='z')
+{
+printf("%c ",alphabet);
+alphabet++;
+}
+return 0;
+}
+
