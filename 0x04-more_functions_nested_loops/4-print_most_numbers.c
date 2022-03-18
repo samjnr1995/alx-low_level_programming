3fb062bda798cae05ba6dba5444d178c370e2a3c
Samjnr1995 <suduu1995@gmail.com>
2022-03-17 18:42:06 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
  * print_most_numbers - a function that prints the numbers
- *
  * Do not print 2 and 4
  * followed by a new line
- * Retur: Always 0
+ * Return: Always 0
  */
 void print_most_numbers(void)
 {
@@ -15,4 +14,4 @@ if (i != '2' && i !='4')
 _putchar(i);
 }
 _putchar('\n');
-
+}
