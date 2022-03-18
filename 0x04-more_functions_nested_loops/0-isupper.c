5b9758dcfddf5b7e21d800155be8b7f4dd9da8bb
Samjnr1995 <suduu1995@gmail.com>
2022-03-17 19:56:15 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,4 +2,20 @@
 /**
  *
  * _is supper - A func that checks for uppercase character
- * @c: An input character
+ * @c: An input characte
+ * Return: 1 if c is uppercase or  otherwise
+ */
+int _isupper(int c)
+{
+char uppercase = 'A';
+int iaupper = 0;
+for (; uppercase <= 'Z'; uppercase++)
+{
+if (c == uppercase)
+{
+isupper = 1;
+break;
+}
+}
+return (isupper);
+}
