c40b3484dbf2941cb8b67a50d68c5f4b3be52fd0
Samjnr1995 <suduu1995@gmail.com>
2022-03-21 11:26:06 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,11 @@
-_putchar
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+int _putchar(char);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
@@ -10,4 +13,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-#include <stdio.h>
+
+#endif
