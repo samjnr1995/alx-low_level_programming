25b20037bd510652b786346c89f82f829ecd2052
Samjnr1995 <suduu1995@gmail.com>
2022-03-20 23:54:01 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+void reset_to_98(int *n);
+{
+int n;
+n = 402;
+_putchar("n=%d\n", n);
+reset_to_98(&n);
+_putchar("n=%d\n", n);
+return (0);
+}
