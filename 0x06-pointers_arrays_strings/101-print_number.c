1897a60f3c68356221de7804e232c867caee2411
Samjnr1995 <suduu1995@gmail.com>
2022-03-24 19:20:25 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -20,10 +20,10 @@ d = m;
 count = 1;
 while (d > 9)
 {
-d/= 10;
+d /= 10;
 count *= 10;
 }
-for (; count >= 1; count /=10)
+for (; count >= 1; count /= 10)
 {
 _putchar(((m / count) % 10) + 48);
 }
