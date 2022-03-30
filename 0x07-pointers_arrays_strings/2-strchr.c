f477e36a88ff6609d7f5105be4c831d7cb2ee02d
samjnr1995 <suduu1995@gmail.com>
2022-03-30 05:40:48 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strch - prints found c
+ * @s: pointer to char 
+ * @c: char params to found 
+ * Return: *s
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+if (*s == c)
+{
+return (s);
+}
+return (0);
+}
