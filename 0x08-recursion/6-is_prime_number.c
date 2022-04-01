c98d043d35b5c35267d1affd551ce80efb31b2e2
samjnr1995 <suduu1995@gmail.com>
2022-03-31 19:23:14 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 int tmp_prime(int n, int i);
 /**
  * divisors - number is prime?
@@ -6,6 +7,7 @@ int tmp_prime(int n, int i);
  * @m: integer params
  * Return: boolean
  */
+
 int divisors(int n, int m)
 {
 if (m % n == 0)
@@ -13,7 +15,8 @@ if (m % n == 0)
 return (0);
 }
 else if (m / 2 > n)
-return (divisors(n + 2, m)0;
+{
+return (divisors(n + 2, m));
 }
 else
 {
