603f6ee8eeee8db2251af080e525837e2df0d591
samjnr1995 <suduu1995@gmail.com>
2022-04-07 10:37:20 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,45 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * _sqrt_recursion - main funct
- * @n: int n
- * Return: int
+ * divr -divide rec
+ * @n: num input
+ * @i: divisor
+ * Description: divide rec
+ *
+ * Return: factor
+ */
+int divr(int n, int i)
+{
+int ii;
+ii = i;
+if (n == 1)
+return (1);
+if (n % ii == 0 && n % (ii * ii) == 0)
+{
+return (ii * divr(n / (ii * ii), i));
+}
+else if (ii < n / 2)
+{
+return (divr(n, i + 1));
+}
+else
+{
+return (-1);
+}
+}
+/**
+ * _sqrt_recursion -get square root wth rec
+ * @n: input num
+ * Description: return natural square root of anumber
+ *
+ * Return: sqrt n
  */
 int _sqrt_recursion(int n)
 {
-return (_sqrt(n, 1));
-}
-
-/**
- * _sqrt - _sqrt_recursion
- * @n: integer paramtr
- * @i: integer parameter
- * Return: sqrt
- */
-int _sqrt(int n, int i)
-{
 if (n < 0)
 return (-1);
-if ((i * i) > n)
-return (-1);
-if (i * i == n)
-return (i);
-return (_sqrt(n i + 1);
+if (n == 0)
+return (0);
+return (divr(n, 2));
 }
+
