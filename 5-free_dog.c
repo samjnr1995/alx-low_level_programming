49691459f69fc4cfb0db273f133d523146c04440
samjnr1995 <suduu1995@gmail.com>
2022-04-11 05:47:23 -0700

    5-free_dog.c

---
+++ 5-free_dog.c
@@ -0,0 +1,16 @@
+nclude "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dog_t
+ */
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+{
+return;
+}
+free(d->name);
+free(d->owner);
+free(d);
+}
