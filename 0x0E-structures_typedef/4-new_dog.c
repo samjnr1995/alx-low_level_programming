2840e0678a2a226bc6c00299b0c98167ecd1f05a
samjnr1995 <suduu1995@gmail.com>
2022-04-11 09:11:20 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,16 +1,44 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 /**
- * free_dog - frees dogs
- * @d: pointer to dog_t
+ * new_dog - makes a new struct dog
+ * @name: his/her name
+ * @age: how old is he
+ * @owner: who are you
+ * Return: new dog
  */
-void free_dog(dog_t *d)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-if (d == NULL)
+unsigned int i, namel, ownerl;
+dog_t *dog;
+if (name == NULL || owner == NULL)
+return (NULL);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+for (namel = 0; name[namel]; namel++)
+;
+namel++;
+dog->name = malloc(namel * (sizeof(char)));
+if (dog->name == NULL)
 {
-return;
+free(dog);
+return (NULL);
 }
-free(d->name);
-free(d->owner);
-free(d);
+for (i = 0; i < namel; i++)
+dog->name[i] = name[i];
+dog->age = age;
+for (ownerl = 0; owner[ownerl]; ownerl++)
+;
+ownerl++;
+dog->owner = malloc(ownerl * (sizeof(char)));
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+for (i = 0; i < ownerl; i++)
+dog->owner[i] = owner[i];
+return (dog);
 }
