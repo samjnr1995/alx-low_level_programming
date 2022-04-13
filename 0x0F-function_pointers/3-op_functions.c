4fe201874a284e44eb1a50dfd247be33be35601d
samjnr1995 <suduu1995@gmail.com>
2022-04-13 06:57:54 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <stdio.h>
 /**
  * op_add - returns the sum of a and b
  * @a: first integer
@@ -11,8 +10,8 @@ int op_add(int a, int b)
 return (a + b);
 }
 /**
- * op_sub - returns difference of a and b
- * a: first integer
+ * op_sub - calculates the difference of two integers
+ * @a: first integer
  * @b: second integer
  * Return: difference of a and b
  */
@@ -21,9 +20,10 @@ int op_sub(int a, int b)
 return (a - b);
 }
 /**
- * op_mul - returns product of a and b
+ * op_mul - calculates the product of two integers
  * @a: first integer
  * @b: second integer
+ *
  * Return: product of a and b
  */
 int op_mul(int a, int b)
@@ -31,18 +31,24 @@ int op_mul(int a, int b)
 return (a * b);
 }
 /**
- * op_div - returns result of the division of a by b
+ * op_div - calculates the division of two integers
  * @a: first integer
  * @b: second integer
- * Return: result of the division of a by b
+ *
+ * Return: result of the division of a and b
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - calculates the remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of a divided b
  */
 int op_mod(int a, int b)
-
 {
-if (!b)
-{
-printf("Error\n"), exit(100);
-}
 return (a % b);
 }
-
